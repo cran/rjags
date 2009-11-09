25ac935b88530332ea2aa07b5b8db9f2f6dc6646
Martyn Plummer <plummer@iarc.fr>
2009-11-09 00:00:00 +0000

    version 1.0.3-12

--- R/unix/progress.bar.R
+++ R/unix/progress.bar.R
@@ -1,11 +1,11 @@
 updatePB <- function(start.iter, end.iter, adapting)
 {
-    tkProgressBar(title=ifelse(adapting, "Adapting","Updating"),
-                  label="Iteration 0", min = start.iter, max=end.iter,
-                  initial=start.iter)
+  tcltk:::tkProgressBar(title=ifelse(adapting, "Adapting","Updating"),
+                        label="Iteration 0", min = start.iter, max=end.iter,
+                        initial=start.iter)
 }
 
 setPB <- function(pb, iter)
 {
-    setTkProgressBar(pb, iter, label=paste("Iteration",iter))
+  tcltk:::setTkProgressBar(pb, iter, label=paste("Iteration",iter))
 }
