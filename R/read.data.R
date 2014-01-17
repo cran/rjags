3d15d389fea5c546d41ec2068bf2cbcbb03fb9d3
Martyn Plummer <plummerm@iarc.fr>
2014-01-17 18:41:23 +0000

    version 3-12

--- R/read.data.R
+++ R/read.data.R
@@ -1,3 +1,19 @@
+#  R package rjags file R/read.data.R
+#  Copyright (C) 2009-2011 Martyn Plummer
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License version
+#  2 as published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  http://www.r-project.org/Licenses/
+#
+
 read.data <- function(file, format=c("jags","bugs"))
 {
     .Deprecated("read.jagsdata", package="rjags")
